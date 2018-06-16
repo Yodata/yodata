fede2a38f3a463b220ff66060cf3d2319617e032
Dave Duran <dave@yodata.io>
2018-06-16 14:45:27 -0700

    docs($dev): add jsdoc tags

--- packages/event-router/eventRouter.js
+++ packages/event-router/eventRouter.js
@@ -12,23 +12,55 @@ class EventRouter extends EventEmitter {
     this._matcher = new Bloomrun()
   }
 
+  /**
+   * adds a route object matcher and handler
+   * @param {object} match
+   * @param {*} handler
+   */
   registerRoute (match, handler) {
     return this._matcher.add(match, handler)
   }
-
+  /**
+   * returns the first match or null if no match is found
+   * @param {*} event
+   * @returns {Function | null}
+   */
   find (event) {
     return this._matcher.lookup(event)
   }
 
+  /**
+   * returns all match handlers
+   * @param {function[]} event
+   */
   findAll (event) {
     return this._matcher.list(event)
   }
 
+  /**
+   * true if the passed event has a matching handler
+   * @param {!object} event
+   * @returns {boolean}
+   */
+  hasRoute (event) {
+    return this.find(event)
+  }
+
+  /**
+   * receives and event object and returns the result or result(event) if result is a function
+   * @param {object} event
+   * @returns {Promise}
+   */
   async next (event) {
     let handler = this.find(event)
     return callHandler(event)(handler)
   }
 
+  /**
+   * receives an event wrapped in an http request and returns the result or result(event)
+   * @param {!object} req
+   * @returns {Promise}
+   */
   async nextHttp (req) {
     let { json } = require('micro')
     let event = await json(req)
