9bb6976737a15df5ee445826b081e92e75b65158
Dave Duran <dave@yodata.io>
2018-06-15 22:39:27 -0700

    fix tests

--- packages/event-router/eventRouter.js
+++ packages/event-router/eventRouter.js
@@ -1,39 +1,39 @@
-const Bloomrun = require("bloomrun");
-const isFunction = require("lodash.isfunction");
-const EventEmitter = require("events");
+const Bloomrun = require('bloomrun')
+const isFunction = require('lodash.isfunction')
+const EventEmitter = require('events')
 
 const callHandler = event => async handler => {
-  return isFunction(handler) ? await handler(event) : handler;
-};
+  return isFunction(handler) ? handler(event) : handler
+}
 
 class EventRouter extends EventEmitter {
-  constructor() {
-    super();
-    this._matcher = new Bloomrun();
+  constructor () {
+    super()
+    this._matcher = new Bloomrun()
   }
 
-  registerRoute(match, handler) {
-    return this._matcher.add(match, handler);
+  registerRoute (match, handler) {
+    return this._matcher.add(match, handler)
   }
 
-  find(event) {
-    return this._matcher.lookup(event);
+  find (event) {
+    return this._matcher.lookup(event)
   }
 
-  findAll(event) {
-    return this._matcher.list(event);
+  findAll (event) {
+    return this._matcher.list(event)
   }
 
-  async next(event) {
-    let handler = this.find(event);
-    return await callHandler(event)(handler);
+  async next (event) {
+    let handler = this.find(event)
+    return callHandler(event)(handler)
   }
 
-  async nextHttp(req) {
-    let { json } = require("micro");
-    let event = await json(req);
-    return this.next(event);
+  async nextHttp (req) {
+    let { json } = require('micro')
+    let event = await json(req)
+    return this.next(event)
   }
 }
 
-module.exports = EventRouter;
+module.exports = EventRouter
