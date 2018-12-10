f670e4b5facf005b9a22cff84442552bc90533c6
Dave Duran <dave@yodata.io>
2018-12-09 20:56:41 -0800

    update package dependencies

--- packages/transform/src/index.js
+++ packages/transform/src/index.js
@@ -1,3 +1,4 @@
 module.exports.Context = require("./context")
 module.exports.keyOrder = require("./plugin/key-order")
 module.exports.defaultValues = require("./plugin/plugin-default-values")
+module.exports.store = require('./schema')
