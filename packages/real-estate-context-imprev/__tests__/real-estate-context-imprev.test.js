64dc2f891172cb24a23d8a59917712504212c8d3
Dave Duran <dave@yodata.io>
2019-01-11 23:01:34 -0800

    add test

--- packages/real-estate-context-imprev/__tests__/real-estate-context-imprev.test.js
+++ packages/real-estate-context-imprev/__tests__/real-estate-context-imprev.test.js
@@ -1,7 +1,11 @@
-'use strict';
+'use strict'
 
-const realEstateContextImprev = require('..');
+const context = require('../lib/real-estate-context-imprev')
 
 describe('@yodata/real-estate-context-imprev', () => {
-    it('needs tests');
-});
+	it('better work', () => {
+		const source = require('../example/real-estate-agent.yaml')
+		const dest = require('../example/external-user.yaml')
+		expect(context.map(source)).toEqual(dest)
+	})
+})
