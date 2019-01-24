7db95dbc50d9e5617374cd9568f42c7e5fe6258f
Dave Duran <dave@yodata.io>
2019-01-24 06:22:29 -0800

    fix(transform-plugin-view): fix named function

--- packages/transform-plugin-view/lib/transform-plugin-view.js
+++ packages/transform-plugin-view/lib/transform-plugin-view.js
@@ -1,12 +1,14 @@
-"use strict";
+'use strict'
+
 const jsonata = require('jsonata')
+
 const TOKEN = '@view'
 const MAP_RESULT = 'MAP_RESULT'
 
-module.exports = function keyOrderPlugin(event, data) {
-  if (event === MAP_RESULT && this.has(TOKEN)) {
+module.exports = function (event, data) {
+	if (event === MAP_RESULT && this.has(TOKEN)) {
 		const expression = jsonata(this.get(TOKEN))
 		return expression.evaluate(data)
-  }
-  return data;
+	}
+	return data
 };
