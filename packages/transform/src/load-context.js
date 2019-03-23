066e269b0c516c4af7acf0648437c536a40520d0
Dave Duran <dave@yodata.io>
2019-03-23 12:17:03 -0700

    ammend

---
+++ packages/transform/src/load-context.js
@@ -0,0 +1,37 @@
+const callerPath = require('caller-path')
+const Yaml = require('js-yaml')
+const Context = require('./context')
+const fs = require('fs-extra')
+const path = require('path')
+
+/**
+ *
+ *
+ * @param {string} filePath
+ * @param {object} [contextOptions]
+ * @returns {Context}
+ */
+module.exports = function loadContext(filePath, contextOptions) {
+	const dirName = path.dirname(callerPath())
+	const target = path.resolve(dirName, filePath)
+	const extName = path.extname(target)
+	let source
+	let cdef
+	switch (extName) {
+		case '.yaml':
+		case '.yml':
+			source = fs.readFileSync(target, 'utf8')
+			cdef = Yaml.load(source)
+			break
+		case '.json':
+			source = fs.readFileSync(target, 'utf8')
+			cdef = JSON.parse(source)
+			break
+		case '.js':
+			cdef = require(target)
+			break
+		default:
+			throw new Error(`unknown file type ${extName}`)
+	}
+	return new Context(cdef, contextOptions)
+}
