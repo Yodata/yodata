a440fb6b5d43800acfdf64899568f512eeb00c6e
Dave Duran <dave@yodata.io>
2019-04-29 13:55:45 -0700

    deploy

---
+++ packages/context-sdk/lib/deploy/cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const { client } = require('@yodata/cli')
+const getContextInfo = require('../info/get-context-info')
+const fs = require('fs')
+
+getContextInfo()
+	.then(info => {
+		const { path, contentType, url } = info
+		const content = fs.readFileSync(path, 'utf8')
+		client.putData(url, {
+			headers: {
+				'content-type': contentType
+			},
+			body: content
+		})
+	})
--- packages/context-sdk/lib/generate-project/generate-project.js
+++ packages/context-sdk/lib/generate-project/generate-project.js
@@ -1,7 +1,9 @@
 const copy = require('./copy-files')
 const installDependencies = require('./install-dependencies')
+const setProfile = require('../set-profile/set-profile')
 
 module.exports = ({ source }) => async function generateProject(props) {
 	return copy(source, props)
 		.then(installDependencies)
+		.then(setProfile)
 }
