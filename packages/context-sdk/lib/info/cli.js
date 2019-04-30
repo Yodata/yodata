bd2c03a78b85e5f14b20a664ed0497a0000f3873
Dave Duran <dave@yodata.io>
2019-04-30 12:15:41 -0700

    fix(cli/context-sdk): update context-sdk template to use latest version of the sdk on install

--- packages/context-sdk/lib/info/cli.js
+++ packages/context-sdk/lib/info/cli.js
@@ -3,9 +3,9 @@ const info = require('.')
 const handler = require('@yodata/cli').createCLIResponseHandler
 
 require('yargs')
+	.scriptName('npx info')
 	.env('YODATA')
 	.option('output', { alias: 'o', describe: 'output', global: true })
-	.command('info', 'returns info', {}, handler(info))
-	.demandCommand()
+	.command('$0', 'return package info', {}, handler(info))
 	.help()
 	.argv
