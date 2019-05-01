c2bfcc014e6962288bf4254a6c898e30c9414ff9
Dave Duran <dave@yodata.io>
2019-05-01 16:35:54 -0700

    fix context-sdk template

--- packages/context-sdk/lib/generate-project/template/__tests__/{{sourceContext}}.test.js
+++ packages/context-sdk/lib/generate-project/template/__tests__/{{sourceContext}}.test.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const { loadContext } = require('@yodata/context-sdk')
 
-test('{{sourceContext}}', () => {
-	const context = loadContext('../{{sourceContext}}.cdef.yaml')
+test('{{context.name}}', () => {
+	const context = loadContext('../{{context.name}}.cdef.yaml')
 	const input = require('../example/input.json')
 	const output = require('../example/output.json')
 	const result = context.map(input)
