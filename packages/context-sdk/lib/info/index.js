a9035647d4d1a5b245b0a7b466287512e212950d
Dave Duran <dave@yodata.io>
2019-05-01 17:04:06 -0700

    =context-sdk/info fix

--- packages/context-sdk/lib/info/index.js
+++ packages/context-sdk/lib/info/index.js
@@ -9,18 +9,22 @@ async function getContextInfo(props = {}) {
 	return readPkgUp()
 		.then(package => {
 			const { environment, filename, context, pod } = props
+			console.log({ environment, filename, context, pod })
 			defaults(context, {
 				name: package.pkg.name,
 				description: package.pkg.description,
-				dirname: path.dirname(package.path)
+				dirname: path.dirname(package.path),
+				contentType: 'application/x-yaml'
 			})
+			console.log({ context })
 			defaults(pod, {
 				url: config.get(`${context.name}.pod.url`) || config.get('default.pod.url'),
 				secret: config.get(`${context.name}.pod.secret`) || config.get('default.pod.secret')
 			})
+			console.log({ pod })
 			context.filename = filename || `${context.name}.cdef.yaml`
 			context.filepath = path.join(context.dirname, context.filename)
-			context.contentType = 'application/x-yaml'
+
 			context.url = `${pod.url}/public/context/`
 			if (environment && environment !== 'production') {
 				context.url += `${environment}/`
