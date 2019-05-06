38d786cb1dba269d01ff7dd49b17da5bf2cbc7ec
Dave Duran <dave@yodata.io>
2019-05-06 08:55:43 -0700

    update cli subscription add

--- packages/cli/lib/command/subscription.js
+++ packages/cli/lib/command/subscription.js
@@ -8,19 +8,30 @@ exports.builder = function (cli) {
 	cli.default('output', 'text')
 	cli.command('ls', 'list', {}, handler(client.subscription.list))
 	cli.command(
-		'add <agent>',
-		'add subscription',
+		'add <agent> [topic]',
+		'adds a subscription for agent',
 		{
 			agent: {
-				description: 'subscriber uri'
+				description: 'subscriber uri',
+				demandOption: true,
+				type: 'string'
 			},
 			topic: {
-				alias: 't',
-				description: 'subscription topic',
-				default: 'realestate/contact'
+				description: 'object shortcut: topic => /event/topic/{topic}',
+				type: 'string'
+			},
+			context: {
+				alias: 'c',
+				description: '(url) messages will be processed by context before delivery',
+				type: 'string'
 			},
 			target: {
-				description: 'subscription target (URL|ARN)'
+				description: 'push events to [target] URL|ARN',
+				type: 'string'
+			},
+			object: {
+				description: 'subscription path',
+				type: 'string'
 			}
 		},
 		handler(client.subscription.add)
