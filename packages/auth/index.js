6ec70067abe5ec9f6b2a8f2435b5e1c64438b660
Dave Duran <dave@yodata.io>
2019-05-18 07:48:50 -0700

    =prune context-realestate package

--- packages/auth/index.js
+++ packages/auth/index.js
@@ -1,48 +1,48 @@
-const { send } = require("micro");
-const compose = require("micro-compose");
-const microAuthGoogle = require("microauth-google");
-const microAuthGithub = require("microauth-github");
+const {send} = require('micro')
+const compose = require('micro-compose')
+const microAuthGoogle = require('microauth-google')
+const microAuthGithub = require('microauth-github')
 
 const githubOptions = {
-	clientId:     process.env.GH_CLIENT_ID,
+	clientId: process.env.GH_CLIENT_ID,
 	clientSecret: process.env.GH_CLIENT_SECRET,
-	callbackUrl:  "http://localhost:3000/auth/github/callback",
-	path:         "/auth/github",
-	scope:        "user"
-};
+	callbackUrl: 'http://localhost:3000/auth/github/callback',
+	path: '/auth/github',
+	scope: 'user'
+}
 
 const googleOptions = {
-	clientId:     "client_id",
-	clientSecret: "client_secret",
-	callbackUrl:  "http://auth.yodata.me/auth/google/callback",
-	path:         "/auth/google",
-	scope:        "identity.basic,identity.team,identity.avatar"
-};
+	clientId: 'client_id',
+	clientSecret: 'client_secret',
+	callbackUrl: 'http://auth.yodata.me/auth/google/callback',
+	path: '/auth/google',
+	scope: 'identity.basic,identity.team,identity.avatar'
+}
 
-const googleAuth = microAuthGoogle(googleOptions);
-const githubAuth = microAuthGithub(githubOptions);
+const googleAuth = microAuthGoogle(googleOptions)
+const githubAuth = microAuthGithub(githubOptions)
 
 const handler = async (req, res, auth) => {
-
 	if (!auth) {
-		return send(res, 404, "Not Found");
+		return send(res, 404, 'Not Found')
 	}
 
 	if (auth.err) {
-		console.error(auth.err);
-		return send(res, 403, "Forbidden");
+		console.error(auth.err)
+		return send(res, 403, 'Forbidden')
 	}
 
-	if (auth.result.provider === "github") {
-		return `${auth.result.provider} provider. Hello ${auth.result.info.login}`;
-	} else if (auth.result.provider === "google") {
-		return `${auth.result.provider} provider. Hello ${auth.result.info.user.name}`;
-	} else {
-		return "Unknown provider";
+	if (auth.result.provider === 'github') {
+		return `${auth.result.provider} provider. Hello ${auth.result.info.login}`
 	}
 
-};
+	if (auth.result.provider === 'google') {
+		return `${auth.result.provider} provider. Hello ${auth.result.info.user.name}`
+	}
+
+	return 'Unknown provider'
+}
 
 module.exports = compose(
 	githubAuth
-)(handler);
+)(handler)
