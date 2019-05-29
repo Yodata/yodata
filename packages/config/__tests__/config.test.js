bc7874630da2a6fc1afeec8c019d69797b61a50f
Dave Duran <dave@yodata.io>
2019-05-29 12:10:00 -0700

    feat(refactor @yodata/config): added config.profile.{get,set,delete,has}

--- packages/config/__tests__/config.test.js
+++ packages/config/__tests__/config.test.js
@@ -1,7 +1,50 @@
-'use strict';
+'use strict'
+const config = require('..')
+const restorekeys = [
+	'profile'
+]
+const restorevalues = []
+const TEST_PROFILE = 'configtest'
 
-const config = require('..');
+beforeAll(() => {
+	restorekeys.forEach(key => {
+		restorevalues.push(config.get(key))
+	})
+	config.set('profile', TEST_PROFILE)
+	config.delete(TEST_PROFILE)
+});
+
+afterAll(() => {
+	restorekeys.forEach((key, index) => {
+		config.set(key, restorevalues[index])
+	})
+});
 
 describe('@yodata/config', () => {
-    it('needs tests');
-});
+	it('should get profile name', () => {
+		expect(config.get('profile')).toEqual(TEST_PROFILE)
+		expect(config.profile.name()).toEqual(TEST_PROFILE)
+		expect(config.all()).toHaveProperty('profile', TEST_PROFILE)
+		expect(config.currentProfile()).toEqual(TEST_PROFILE)
+	});
+	it('should validate profile value', () => {
+		expect(() => config.set('profile', undefined)).toThrow()
+		expect(() => config.setProfile(undefined)).toThrow()
+		expect(() => config.profile.use()).toThrow()
+		expect(() => config.set('profile.foo', 'bar')).toThrow()
+	});
+	it('should get and set profile subvalues', () => {
+		config.delete('default.foo')
+		config.profile.delete('foo')
+		expect(config.profile.get('foo')).toBeUndefined()
+		expect(config.profile.set('foo', 'bar')).toEqual('bar')
+		expect(config.profile.get('foo')).toEqual('bar')
+	});
+	it('should return default values', () => {
+		config.delete('default.foo.bar')
+		expect(config.profile.get('foo.bar')).toBeUndefined()
+		expect(config.set('default.foo.bar', 'bat')).toEqual('bat')
+		expect(config.profile.get('foo.bar')).toEqual('bat')
+	});
+
+})
--- packages/config/lib/config.js
+++ packages/config/lib/config.js
@@ -1,8 +1,5 @@
-'use strict';
-
+'use strict'
 const Store = require('configstore')
-const defaults = require('lodash/defaults')
-const logger = console
 const packageName = '@yodata/core'
 const programDefaults = {
 	profile: process.env.YODATA_PROFILE || 'default',
@@ -16,53 +13,149 @@ const programDefaults = {
 const store = new Store(packageName, programDefaults)
 
 exports.all = () => store.all
-exports.get = getValue
-exports.set = setValue
-exports.delete = deleteValue
+exports.currentProfile = getCurrentProfile
+exports.delete = removev
+exports.get = getv
 exports.getCurrentProfile = getCurrentProfile
-exports.getProfileValue = getProfileValue
-exports.has = key => store.has(key)
-exports.profileHas = key => store.has(profileKey(key))
-exports.profileGet = key => store.get(profileKey(key))
-exports.profileSet = (key, value) => store.set(profileKey(key), value)
-exports.profileDelete = key => store.delete(profileKey(key))
+exports.getProfileValue = getpv
+exports.has = hasv
+exports.profileDelete = removepv
+exports.profileGet = getpv
+exports.profileHas = haspv
+exports.profileSet = setpv
+exports.set = setv
+// @ts-ignore
+exports.setProfile = setv('profile')
+exports.profile = {
+	name: getCurrentProfile,
+	get: getpv,
+	set: setpv,
+	delete: removepv,
+	has: haspv,
+	// @ts-ignore
+	use: setv('profile')
+}
 
-function getProfileValue(key, defaultValue) {
-	if (store.has(profileKey(key))) {
-		return store.get(profileKey(key))
+function storeHasProfile() {
+	const profileName = store.get('profile')
+	return (
+		store.has('profile') &&
+		typeof profileName === 'string' &&
+		profileName.length > 0
+	)
+}
+
+function validProfileName(value) {
+	return (
+		typeof value === 'string' &&
+		value.length > 0
+	)
+}
+
+const noProfile = () => !(storeHasProfile())
+
+function pkey(key, profileOverride) {
+	const profile = profileOverride || store.get('profile')
+	return key ? `${profile}.${key}` : profile
+}
+
+
+/**
+ * Returns the name of the current profile.
+ * @returns {string} name of the current profile
+ */
+function getCurrentProfile() {
+	return getv('profile', 'default')
+}
+
+/**
+ * Remove a key from the store.
+ *
+ * @param {string} key - the key to delete
+ * @returns {boolean} true if the item was deleted sucessfully
+ */
+function removev(key) {
+	store.delete(key)
+	return true
+}
+
+/**
+ * Remove a profile key from the store.
+ *
+ * @param {string} key - the key to delete
+ * @returns {boolean} true if the item was deleted sucessfully
+ */
+function removepv(key) {
+	return removev(pkey(key))
+}
+
+/**
+ * Retrieve a value from the store.
+ *
+ * @param {string} key - key to get
+ * @param {*} [defaultValue] - value to return if the value is not in the store.
+ * @returns {any}
+ */
+function getv(key, defaultValue) {
+	return store.has(key) ? store.get(key) : defaultValue
+}
+
+/**
+ * Get profile vaule
+ * @param {string} key - key to get
+ * @param {*} [defaultValue] - default value
+ */
+function getpv(key, defaultValue) {
+	const pk = pkey()
+
+	if (store.has(pkey(key))) {
+		return store.get(pkey(key))
 	}
 
 	if (store.has(`default.${key}`)) {
 		return store.get(`default.${key}`)
 	}
 
-	logger.error('config:not-found:${key}')
+	return defaultValue
+
 }
 
-function getCurrentProfile() {
-	const key = store.has('profile') ? store.get('profile') : 'default'
-	return store.get(key)
-}
-
-function deleteValue(key) {
-	store.delete(key)
-	return `${key} deleted.`
-}
-
-function getValue(key, defaultValue) {
-	if (store.has(key)) {
-		return store.get(key)
+/**
+ * Set config key to value.
+ *
+ * @param {string} key - key to set
+ * @param {any} value - value to set
+ * @returns {function|*} - setter function or value
+ */
+function setv(key, value) {
+	if (arguments.length === 1) {
+		return value => setv(key, value)
+	}
+	// validation: profile must be string
+	if ((key === 'profile' && typeof value !== 'string') || key.startsWith('profile.') ) {
+		throw new TypeError('profile must be a string value')
 	}
 
-	logger.debug('config:not-found', { key })
-}
-
-function setValue(key, value) {
 	store.set(key, value)
-	return `${key} = ${value}`
+	return value
 }
 
-function profileKey(key) {
-	const profile = store.has('profile') ? store.get('profile') : 'default'
-	return `${profile}.${key}`
+
+/**
+ * Set profile value
+ *
+ * @param {string} key - key to set
+ * @param {any} value - value to set
+ */
+function setpv(key, value) {
+	return setv(pkey(key), value)
+}
+
+
+function hasv(key) {
+	return store.has(key)
+}
+
+function haspv(key) {
+	return hasv(pkey(key))
 }
