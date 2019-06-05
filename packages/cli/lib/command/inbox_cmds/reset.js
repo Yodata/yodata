2b074ee9329c836a37844cacef3b8f3d6fb08499
Dave Duran <dave@yodata.io>
2019-06-04 21:38:11 -0700

    feat(cli.inbox.reset command): add yodata inbox reset cmd

--- packages/cli/lib/command/inbox_cmds/reset.js
+++ packages/cli/lib/command/inbox_cmds/reset.js
@@ -10,14 +10,45 @@ exports.builder = {
 		default: false,
 		group: 'reset',
 		desc: 'Removes inbox cache for the current page (forcing your next inbox ls to reload from the source)'
+	},
+	cache: {
+		type: 'boolean',
+		default: false,
+		group: 'reset',
+		desc: 'Clears all cached values'
+	},
+	all: {
+		type: 'boolean',
+		default: false,
+		group: 'reset',
+		desc: 'Removes all settings and clears all cached data'
 	}
 }
 exports.handler = function (argv) {
-	const { last } = argv
+	const { last, cache, all } = argv
+	const profileName = profile.name()
+
 	if (last === true) {
 		const lastv = profile.get('inbox.last')
 		if (typeof lastv === 'string' && lastv.length > 0) {
 			profile.delete(`cache.${lastv}`)
 		}
+	}
+
+	if (cache === true) {
+		profile.delete('cache')
+		console.log(`${profileName} cache deleted.`)
+	}
+
+	if (all === true) {
+		const lastv = profile.get('inbox.last')
+
+		if (typeof lastv === 'string' && lastv.length > 0) {
+			profile.delete(`cache.${lastv}`)
+			console.log(`${profileName} inbox.last deleted`)
+		}
+
+		profile.delete('cache')
+		console.log(`${profileName} cache deleted.`)
 	}
 }
