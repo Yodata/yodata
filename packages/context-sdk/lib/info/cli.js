b9afb08535ffc619baf09f46e9433dcf3d12f5b4
Dave Duran <dave@yodata.io>
2019-06-09 19:46:15 -0700

    fix(context-sdk/info): fixed cli

--- packages/context-sdk/lib/info/cli.js
+++ packages/context-sdk/lib/info/cli.js
@@ -1,16 +1,2 @@
 #!/usr/bin/env node
-
-/* eslint-disable no-unused-expressions */
-
-const info = require('../info')
-require('yargs')
-  .scriptName('npx info')
-  .env('YODATA')
-  .option('output', { alias: 'o', describe: 'output', global: true })
-  .command('$0', 'return package info', {}, infoCommand)
-  .help()
-  .argv
-
-function infoCommand (args) {
-  console.log(info(args))
-}
+console.log(require('./index'))
--- packages/context-sdk/lib/info/index.js
+++ packages/context-sdk/lib/info/index.js
@@ -1,72 +1,22 @@
 'use strict'
-
+/* eslint-disable no-unused-expressions */
+'use strict'
+require('dotenv').config()
 const path = require('path')
-const url = require('url')
 
-const logger = require('@yodata/logger')
-const readPkgUp = require('read-pkg-up')
-const defaults = require('lodash/defaults')
-const getIn = require('lodash/get')
+module.exports = () => {
+  const contextName = process.env.YODATA_PROFILE
+  const context = {
+    name: process.env.YODATA_PROFILE,
+    contentType: 'application/x-yaml',
+    environment: process.env.YODATA_STAGE || 'staging',
+    filename: `${contextName}.cdef.yaml`
+  }
+  context.filepath = path.join(process.cwd(), context.filename)
 
-module.exports = getContextInfo
-
-/**
- * @typedef ContextInfoObject
- * @property {object} [context]
- * @property {string} [context.$schema]
- * @property {string} [context.name]
- * @property {string} [context.description]
- * @property {string} [context.dirname]
- * @property {string} [context.contentType]
- * @property {string} [context.filename]
- * @property {string} [context.filepath]
- * @property {string} [context.url]
- * @property {object} [pod]
- * @property {string} pod.url
- * @property {string} pod.secret
- * @property {string} [environment]
- *
- *
- *
- * get attributes of the current context
- *
- * @param {object} [props]
- * @param {object} [props.context]
- * @param {object} [props.pod]
- * @param {string} [props.environment]
- * @param {string} [props.filename]
- * @returns {Promise<ContextInfoObject>}
- */
-async function getContextInfo (props = {}) {
-  const { environment = 'stage', filename, context = {}, pod = {} } = props
-  return readPkgUp()
-    .then(data => {
-      defaults(context, {
-        name: getIn(data, 'pkg.name'),
-        description: getIn(data, 'pkg.description'),
-        contentType: 'application/x-yaml',
-        environment: environment || 'stage'
-      })
-      defaults(pod, {
-        url: process.env.YODATA_POD_URL,
-        secret: process.env.YODATA_POD_SECRET
-      })
-
-      context.dirname = (data && data.path) ? path.dirname(data.path) : path.join(process.cwd(), context.name)
-      context.filename = filename || `${context.name}.cdef.yaml`
-      context.filepath = path.join(context.dirname, context.filename)
-
-      const segment = ['/public/context']
-      if (typeof context.environment === 'string' && context.environment !== 'production') {
-        segment.push(context.environment)
-      }
-
-      segment.push(context.filename)
-      context.url = url.resolve(pod.url, path.join(...segment))
-      return { ...props, context, pod }
-    })
-    .catch(error => {
-      logger.error(error.message, error)
-      throw new Error(error.message)
-    })
+  context.hostname = process.env.YODATA_POD_URL
+  context.hostpath = path.join('/public/context', context.environment, context.filename)
+  context.url = new URL(context.hostpath, context.hostname).href
+  context.hostkey = process.env.YODATA_POD_SECRET
+  return context
 }
