d4feec8479f8eec2fedebb3216b35fb9800f3726
Dave Duran <dave@yodata.io>
2019-06-12 11:10:27 -0700

    update log level

--- packages/client/http/log-request.js
+++ packages/client/http/log-request.js
@@ -3,6 +3,6 @@ const logger = require('@yodata/logger')
 
 module.exports = request => {
   // @ts-ignore
-  logger.info(`${chalk.blue(request.method)} ${chalk.green(request.href)}\n`)
+  logger.debug(`${chalk.blue(request.method)} ${chalk.whiteBright(request.href)}\n`)
   return request
 }
