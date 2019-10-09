a01dfa358ded08a3cdd49397145d427db6ac17d3
Dave Duran <dave@yodata.io>
2019-10-08 19:40:06 -0700

    =remove stub fetch-data

--- packages/fetch-data/__tests__/fetch-data.test.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const fetchData = require('..')
-
-describe('@yodata/fetch-data', () => {
-  it('needs tests')
-})
--- packages/fetch-data/lib/fetch-data.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = fetchData;
-
-function fetchData() {
-    // TODO
-}
