020a3726168b1f4a35bb47be5e9f947b72d989ab
David Duran <dave@ddofficeimac.local>
2019-11-08 14:17:18 -0800

    fix logger defect

--- packages/logger/lib/logger.js
+++ packages/logger/lib/logger.js
@@ -1,70 +1,29 @@
-'use strict'
-const logger = require('loglevel')
-const { inspect } = require('util')
 
-const originalFactory = logger.methodFactory
+const { format, transports, createLogger } = require('winston')
+const { timestamp, printf, metadata } = format
 
-logger.methodFactory = function (methodName, logLevel, loggerName) {
-  const rawMethod = originalFactory(methodName, logLevel, loggerName)
-  return function (message, data, options = {}) {
-    const messages = []
-    if (typeof message === 'object') {
-      data = message
-      message = data.message || levelName()
-    }
-    messages.push(message)
+const aws = format.combine(
+  metadata({ key: 'data' }),
+  timestamp({
+    format: 'YYYY-MM-DD HH:mm:ss'
+  }),
+  printf(info => `${info.timestamp} [${info.level}] ${info.message} ${JSON.stringify(info.data)}`)
+)
 
-    const compact = options[ 'compact' ] || true
-    const depth = Number(options[ 'depth' ] || process.env.LOG_DEPTH || '5')
-    if (typeof data === 'object') {
-      messages.push(inspect(data, { ...options, compact, depth }))
-    }
+const defaultLogger = createLogger({
+  format: aws,
+  level: process.env.LOG_LEVEL || 'info',
+  transports: [
+    new transports.Console({ handleExceptions: true })
+  ]
+})
 
-    rawMethod.apply(undefined, messages)
-  }
+defaultLogger.createLogger = createLogger
+defaultLogger.table = console.table
+defaultLogger.tap = (message, level) => data => {
+  const log = defaultLogger[ level ] || defaultLogger
+  log(message, data)
 }
+defaultLogger.trace = defaultLogger.debug
 
-const logValue = (message, level = getDefaultLogLevel()) => data => {
-  logger[ level ](message, data)
-  return data
-}
-
-const levelName = () => {
-  return Object.keys(logger.levels)[ logger.getLevel() ]
-}
-
-function getDefaultLogLevel () {
-  let level = process.env.LOG_LEVEL || process.env.NODE_ENV === 'production' ? 'error' : 'info'
-  return logger.levels[ String(level).toUpperCase() ] || 4
-}
-
-function pretty (message, data, options = {}) {
-  const level = levelName()
-  const depth = 10
-  const compact = false
-  logger[ level ](message, data, { ...options, compact, depth })
-}
-
-logger.setLevel(getDefaultLogLevel())
-
-exports.createLogger = logger.getLogger
-exports.debug = logger.debug
-exports.error = logger.error
-exports.info = logger.info
-exports.log = logger.info
-exports.table = console.table
-exports.trace = logger.trace
-exports.warn = logger.warn
-exports.getLevel = logger.getLevel
-exports.setLevel = logger.setLevel
-exports.setDefaultLevel = logger.setDefaultLevel
-exports.levels = logger.levels
-exports.methodFactory = logger.methodFactory
-exports.levelName = levelName
-exports.pretty = pretty
-exports.echo = function () {
-  logger.error(...arguments)
-  return arguments
-}
-exports.tap = logValue
-exports.value = logValue
+module.exports = defaultLogger
