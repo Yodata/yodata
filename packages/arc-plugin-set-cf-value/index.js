20888af6d1f4c025ed3afe5de1fe4f4f550fc404
Dave Duran <dave@yodata.io>
2020-10-18 16:22:52 -0700

    arc-set-cf-value supports stage replace string

--- packages/arc-plugin-set-cf-value/index.js
+++ packages/arc-plugin-set-cf-value/index.js
@@ -3,6 +3,9 @@ const setvalue = require('set-value')
 module.exports = function setcloudformationvalue (arc, cf, stage) {
   const values = arc['set-cf-values'] || []
   values.forEach(([key, value]) => {
+    if (String(value).includes('{stage}')) {
+      value = value.replace('{stage}', stage)
+    }
     setvalue(cf, key, value)
   })
   return cf
