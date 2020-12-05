974666035425ab72609efeede175b8c3bcc8a371
Dave Duran <dave@yodata.io>
2020-12-05 14:57:07 -0800

    client updates for got library breaking changes

--- packages/client/http/log-request.js
+++ packages/client/http/log-request.js
@@ -1,6 +1,7 @@
+const logger = require('@yodata/logger')
 
 module.exports = request => {
   // @ts-ignore
-  // logger.debug(`${chalk.blue(request.method)} ${chalk.whiteBright(request.url.href)}\n`)
-  return request
+  logger.debug(`${request.method} ${request.url.href}\n`)
+  // return request
 }
